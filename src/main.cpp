70c4dc967ac81b8f7862c52fffdfe67bccff07b8
andreox <andreozzi.alessio@gmail.com>
2014-02-02 20:08:38 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,23 +9,28 @@ int main( int argc , char** argv ) {
 	Fraction f1( 2 , 2) ;
 	Fraction f2( 2 , 3) ;
 
+	f1.Print() ; cout << " + " ; f2.Print() ; cout << " = " ;
 	f1 = f1+f2 ;
-	f1.Print() ;
+	f1.Print() ; cout << endl ;
 
+	f1.Print() ; cout << " - " ; f2.Print() ; cout << " = " ;
 	f1 = f1-f2 ;
-	f1.Print() ;
+	f1.Print() ; cout << endl ;
 
+	f1.Print() ; cout << " * " ; f2.Print() ; cout << " = " ;
 	f1 = f1*f2 ;
-	f1.Print() ;
+	f1.Print() ; cout << endl ;
 
+	f1.Print() ; cout << " / " ; f2.Print() ; cout << " = " ;
 	f1 = f1/f2 ;
-	f1.Print() ;
+	f1.Print() ; cout << endl ;
 
+	cout << "(" ; f1.Print() ; cout << ")³" ; cout << " = " ;
 	f1 = f1^3 ;
-	f1.Print() ;
+	f1.Print() ; cout << endl ;
 
 	f1.Reduce() ;
-	f1.Print() ;
+	f1.Print() ; cout << endl ;
 
 	return 0 ;
 
